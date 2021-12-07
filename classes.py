d4df6e032dcbe219eb45b83c3cf7f30e7d23f19c
belmahdi zakaria borhane eddine <setif.coin.dz@gmail.com>
2021-12-07 16:58:54 +0100

    added the point class

--- classes.py
+++ classes.py
@@ -1,27 +1,8 @@
-class Flight():
-    def __init__(self,capacity):
-        self.capacity =capacity
-        self.passengers = []
-   
-   
-    def add_passenger(self,name):
-        if not self.open_seats():
-            return False
-        self.passengers.append(name)
-        return True
-  
-    def open_seats(self):
-        return self.capacity - len(self.passengers)
+class Point():
+    def __init__(self,input1 ,input2):
+        self.x = input1
+        self.y = input2
+p= Point(5,6)
 
-
-capacity= input("insert the number of available seats :")
-
-flight = Flight(int(capacity))
-people = ["zaki","younes","loulou","zineb"]
-
-for person in people:
-    success = flight.add_passenger(person)
-    if success :
-        print(f"added {person} to the flight succesfully")
-    else :
-        print(f"no vaialable seats for {person}")
+print(p.x)
+print(p.y)
