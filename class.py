8f58311e9e1245529d71e59eb3388e4736c2d397
belmahdi zakaria borhane eddine <setif.coin.dz@gmail.com>
2021-12-07 17:50:55 +0100

    add a new class to find error

---
+++ class.py
@@ -0,0 +1,27 @@
+from os import name
+
+
+class Flight():
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.passengers = []
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+    def add_passenger(self,name):
+        if not open_seats():
+            return False
+        else:
+            self.passengers.append(name)
+            return True
+
+
+flight = Flight(3)
+
+people =["zaki","borhan","belmahdi","algeria"]
+
+for person in people:
+   sucess = flight.add_passenger(name)
+   if sucess:
+       print(f"the person {person} hass been added succcessfully")
+   else:
+        print(f"nor available seats for {person}")
